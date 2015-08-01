7c9befee928e6c69cb088aebc4f02b57c210fa8a
alexan1 <alexan1@gmail.com>
2015-07-31 22:55:26 -0400

    CDN and scripts

---
+++ SignalRChat/Scripts/signalr-start.js
@@ -0,0 +1,8 @@
+﻿// Declare a proxy to reference the hub.
+//var url = window.location.href;
+//trace('url = ' + url);
+var scripts = document.getElementsByTagName("script"),
+    src = toLocation(scripts[scripts.length - 1].src).origin;
+$.connection.hub.url = src + "/signalr";
+trace('connection url = ' + $.connection.hub.url);
+var chat = $.connection.chatHub;
