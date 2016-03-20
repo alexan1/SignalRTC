6e8b236fdb0b5af563b85fed4f83a7c6a85630a9
Alexandre Konioukhov <alexan1@gmail.com>
2016-03-20 18:07:18 -0400

    login test

--- TestWeb/Scripts/utils.js
+++ TestWeb/Scripts/utils.js
@@ -21,6 +21,20 @@ else {
         $myModal.modal('show');
     };
 };
+
+function onSignIn(googleUser) {
+    // Useful data for your client-side scripts:
+    var profile = googleUser.getBasicProfile();
+    console.log("ID: " + profile.getId()); // Don't send this directly to your server!
+    console.log("Name: " + profile.getName());
+    console.log("Image URL: " + profile.getImageUrl());
+    console.log("Email: " + profile.getEmail());
+
+    // The ID token you need to pass to your backend:
+    var id_token = googleUser.getAuthResponse().id_token;
+    console.log("ID Token: " + id_token);
+};
+
 function generateQuickGuid() {
     return Math.random().toString(36).substring(2, 15);       
 }
