b5df1d32ec13a8ff5cb01e0f4905b076d11f75cf
Alexandre Konioukhov <alexan1@gmail.com>
2019-05-20 08:48:06 -0400

    removed TestWeb

--- TestWeb/Scripts/_reference.js
+++
@@ -1,5 +0,0 @@
-﻿/// <reference path="bootstrap.js" />
-/// <reference path="jquery-1.9.1.js" />
-/// <reference path="signalr.js" />
-/// <reference path="utils.js" />
-/// <reference path="video.js" />
